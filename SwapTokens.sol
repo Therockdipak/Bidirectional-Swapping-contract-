57b1da0393865122539b6219eb04bfcccd6fff7d
Dipak Bansod <108859739+Therockdipak@users.noreply.github.com>
2024-05-12 20:47:07 +0530

    Update SwapTokens.sol

--- SwapTokens.sol
+++ SwapTokens.sol
@@ -1,71 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-import {AToken} from "./TokenA.sol";
-import {BToken} from "./TokenB.sol";
-
- contract TokenSwap is Ownable {
+contract TokenSwap is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    AToken public tokenA;
-    BToken public tokenB;
+    IERC20 public tokenA;
+    IERC20 public tokenB;
 
     uint256 public rate = 100;  // 1A = 100B
     uint256 public purchaseCount;
     uint256 public purchaseThreshold = 10000;
     uint256 public rateIncreasePercentage = 10;
+    uint256 public initialBPrice = 0.01 ether; // Initial price of B token
 
     event TokensSwapped(address indexed buyer, uint256 amountA, uint256 amountB);
+    event tokenSwapped(address indexed buyer, uint256 amountB, uint256 amountA);
 
-    constructor(AToken _tokenA, BToken _tokenB) Ownable(msg.sender) {
+    constructor(IERC20 _tokenA, IERC20 _tokenB) Ownable(msg.sender) {
         tokenA = _tokenA;
         tokenB = _tokenB;
     }
 
-    function initializeTokenB(address _tokenBAddress) external onlyOwner {
-        require(address(tokenB) == address(0), "TokenB address already set");
-        tokenB = BToken(_tokenBAddress);
+    function swapAToB(uint256 _amountA) external nonReentrant {
+        uint256 amountB = _amountA * rate;
+        require(tokenA.balanceOf(msg.sender) >= _amountA, "Insufficient TokenA balance");
+        require(tokenB.balanceOf(address(this)) >= amountB, "Not enough tokens in contract");
+
+        tokenA.safeTransferFrom(msg.sender, address(this), _amountA);
+        tokenB.safeTransfer(msg.sender, amountB);
+
+        emit TokensSwapped(msg.sender, _amountA, amountB);
+
+        updateRates(amountB);
     }
 
-    function tokenBTransfer(uint256 _amount) external onlyOwner{
-        require(tokenB.balanceOf(msg.sender) >= _amount, "Insufficient TokenB balance");
+    function swapBToA(uint256 _amountB) external nonReentrant {
+        uint256 amountA = _amountB / rate;
+        require(tokenB.balanceOf(msg.sender) >= _amountB, "Insufficient TokenB balance");
+        require(tokenA.balanceOf(address(this)) >= amountA, "Not enough tokens in contract");
 
-        IERC20(tokenB).safeTransferFrom(msg.sender, address(this), _amount);
+        tokenB.safeTransferFrom(msg.sender, address(this), _amountB);
+        tokenA.safeTransfer(msg.sender, amountA);
 
-        emit TokensSwapped(msg.sender, 0, _amount);
+        emit tokenSwapped(msg.sender, _amountB, amountA);
 
-        // Check if the purchase threshold is crossed
+        updateRates(_amountB);
+    }
+
+    function updateRates(uint256 _amount) internal {
+        purchaseCount += _amount;
         if (purchaseCount >= purchaseThreshold) {
             rate = rate + (rate * rateIncreasePercentage / 100);
             purchaseCount = 0;
-        } else {
-            purchaseCount += _amount;
+            initialBPrice = initialBPrice + (initialBPrice * rateIncreasePercentage / 100);
         }
     }
 
-    function swapTokens(uint256 amountA) external {
-        require(tokenA.balanceOf(msg.sender) >= amountA, "Insufficient TokenA balance");
+    function withdrawTokenA(uint256 _amount) external onlyOwner {
+        tokenA.safeTransfer(msg.sender, _amount);
+    }
 
-        uint256 amountB = amountA * rate;
+    function withdrawTokenB(uint256 _amount) external onlyOwner {
+        tokenB.safeTransfer(msg.sender, _amount);
+    }
 
-        // Perform the token swap
-        IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
-        IERC20(tokenB).transfer(msg.sender, amountB);
-        console.log(msg.sender);
-        emit TokensSwapped(msg.sender, amountA, amountB);
+    function setPurchaseThreshold(uint256 _threshold) external onlyOwner {
+        purchaseThreshold = _threshold;
+    }
 
-        // Check if the purchase threshold is crossed
-        if (purchaseCount >= purchaseThreshold) {
-            rate = rate + (rate * rateIncreasePercentage / 100);
-            purchaseCount = 0;
-        } else {
-            purchaseCount += amountB;
-        }
+    function setRateIncreasePercentage(uint256 _percentage) external onlyOwner {
+        rateIncreasePercentage = _percentage;
     }
 }
